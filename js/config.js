9d112930bb5a0e89183a59c5235c1ae315b14696
tsw6 <thetswe@gmail.com>
2016-11-23 17:56:27 +0800

    Remove data-tweet-limit so that the data poll can happen

--- js/config.js
+++ js/config.js
@@ -8,8 +8,8 @@ var config = {
 	apiKey: 3,
 	apiSecret: "94CUqFtsoTmiKdYPyV3pvK7h7GxF0tgXMJyMNtbH3qDfx3kOASdX1ECz1cJraeGr",
 	bus : {
-		stop: 12109,
-		name: "Opp Ngee Ann Poly",
-		buses: "52,61,74,75,151,154,184"
+		stop: 42071, //12109,
+		name: "Shell Kiosk", //"Opp Ngee Ann Poly",
+		buses: "41,52,61,66,67,75,77,157,170,171,174,184,852,961,970,985" //"52,61,74,75,151,154,184"
 	}
 }
