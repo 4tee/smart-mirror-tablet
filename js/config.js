66cb3a2177d27bf9c6fe8ebbc4c43afdb5c64c69
tsw6 <thetswe@gmail.com>
2016-11-29 10:32:12 +0800

    update server url in config

--- js/config.js
+++ js/config.js
@@ -4,12 +4,12 @@ var config = {
 		name: "Singapore, Singapore", //For default weather, map
 		zoom: 13
 	},
-	busURL: "http://172.18.102.148:9999",
+	busURL: "http://172.18.97.110:9999",
 	apiKey: 3,
 	apiSecret: "94CUqFtsoTmiKdYPyV3pvK7h7GxF0tgXMJyMNtbH3qDfx3kOASdX1ECz1cJraeGr",
 	bus : {
-		stop: 42071, //12109,
-		name: "Shell Kiosk", //"Opp Ngee Ann Poly",
-		buses: "41,52,61,66,67,75,77,157,170,171,174,184,852,961,970,985" //"52,61,74,75,151,154,184"
+		stop: 12109, //42071,
+		name: "Opp Ngee Ann Poly", //"Shell Kiosk",
+		buses: "52,61,74,75,151,154,184" //"41,52,61,66,67,75,77,157,170,171,174,184,852,961,970,985"
 	}
 }
